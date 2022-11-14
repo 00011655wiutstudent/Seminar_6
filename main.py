9e206cdd5a8d4fe117a8286ca8060145fc5d8223
00011655wiutstudent <00011655wiutstudent@gmail.com>
2022-11-14 17:12:26 +0500

    Seminar 6, task 5 is complete

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Task 4
+
 def test_is_valid(test):
     if isinstance(test, int) and test in range(1, 4):
         print("True")
@@ -7,3 +8,14 @@ def test_is_valid(test):
 
 
 test_is_valid('fdj')
+# Task 5
+def find(word, letter, index):
+    while index < len(word):
+        if word[index] == letter:
+            return index
+        index = index + 1
+    return -1
+
+
+print(find("computer", "u", 5))
+
